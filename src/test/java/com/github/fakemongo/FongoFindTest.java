c03d6eb5353dd6fd540d5ea767f9045a60a2e82a
Alban Dericbourg <alban.dericbourg@darty.fr>
2014-05-16 13:50:02 +0200

    fix #40 find() with projection from array.
    Using a projection from an array in find() returned only the last projection (and not all the field).

--- src/test/java/com/github/fakemongo/FongoFindTest.java
+++ src/test/java/com/github/fakemongo/FongoFindTest.java
@@ -1,14 +1,17 @@
 package com.github.fakemongo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import com.github.fakemongo.junit.FongoRule;
-import org.junit.Rule;
-import org.junit.Test;
-
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import org.junit.Rule;
+import org.junit.Test;
 
 public class FongoFindTest {
 
@@ -28,4 +31,29 @@ public class FongoFindTest {
     assertEquals(new BasicDBObject("_id", 1).append("field", 12L), result);
   }
 
+
+  /**
+   * Checks that specified fields in find()'s projection from a list are actually returned (and are the only returned).
+   */
+  @Test
+	public void testListFieldsProjection() {
+    // Given
+    DBCollection collection = fongoRule.newCollection();
+    BasicDBList list = new BasicDBList();
+    list.add(new BasicDBObject("a", "1").append("b", "2").append("c", "3"));
+    list.add(new BasicDBObject("a", "4").append("b", "5").append("d", "6"));
+    collection.insert(new BasicDBObject("_id", 1).append("lst", list));
+
+    // When
+    DBCursor cursor = collection.find(new BasicDBObject(), new BasicDBObject("lst.a", 1).append("lst.b", 1));
+
+    // Then
+    BasicDBList lst = (BasicDBList) cursor.next().get("lst");
+    for (Object o : lst) {
+      DBObject item = (DBObject) o;
+      assertNotNull("'a' is expected from projection", item.get("a"));
+      assertNotNull("'b' is expected from projection", item.get("b"));
+      assertNull("'c' is not expected since it is not in projection", item.get("c"));
+    }
+  }
 }
