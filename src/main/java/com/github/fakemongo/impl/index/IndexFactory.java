46474459e6d03beb907d90c7b41aadc4b774cc97
James Jory <james@jamesjory.com>
2014-09-16 15:51:35 -0700

    Only 2d index is required to first in compound index (remove requirement that 2dsphere being first too).

--- src/main/java/com/github/fakemongo/impl/index/IndexFactory.java
+++ src/main/java/com/github/fakemongo/impl/index/IndexFactory.java
@@ -45,8 +45,7 @@ public final class IndexFactory {
     for (Map.Entry<String, Object> entry : Util.entrySet(keys)) {
       Object value = entry.getValue();
       if (value instanceof String) {
-        boolean localGeo = "2d".equals(value) || "2dsphere".equals(value);
-        if (localGeo) {
+        if ("2d".equals(value)) {
           if (!first) {
             //	"err" : "2d has to be first in index", "code" : 13023, "n" : 0, "connectionId" : 206, "ok" : 1
             throw new MongoException(13023, "2d has to be first in index");
